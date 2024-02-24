47a3cd8b855f5efd75d424116f1a50cb09e3071d
Kartik Pal <kartik.pal@konverge.ai>
2024-02-24 17:21:11 +0530

    feat: added api-server

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -3,12 +3,26 @@ const httpProxy = require("http-proxy");
 
 const PORT = 8000;
 const app = express();
+const proxy = httpProxy.createProxy();
 
+
+const BASE_PATH = `https://vercel-clone-output-bucket.s3.ap-south-1.amazonaws.com/__output__`
+
+/* Resolve all request on http://subdomain.localhost:8000 to s3 bucket  */ 
 app.use((req, res) => {
   const hostName = req.hostname;
   const subDomain = hostName.split(".")[0];
+  const resolvesTo = `${BASE_PATH}/${subDomain}`
 
-  const resolvesTo = ''
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
 });
 
-app.listen(PORT, () => `Revese proxy running on port ${PORT}`);
+/* All request on / resolves to index.html */ 
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const url = req.url;
+    if (url === '/')
+        proxyReq.path += 'index.html'
+
+})
+
+app.listen(PORT, () => console.log(`Revese proxy running on port ${PORT}`));
