8a34d4616cdd702142a77fff97dfc670358cd811
Kartik <kartik.p1110@gmail.com>
2024-03-19 23:52:26 +0530

    feat: added login page

--- client-v2/src/app/signup/page.tsx
+++ client-v2/src/app/signup/page.tsx
@@ -32,13 +32,13 @@ export default function AuthenticationPage() {
       </div>
       <div className="container relative hidden h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          href="/signup"
+          href="/login"
           className={cn(
             buttonVariants({ variant: "secondary" }),
             "absolute right-4 top-4 md:right-8 md:top-8"
           )}
         >
-          Sign Up
+          Login
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
@@ -71,10 +71,10 @@ export default function AuthenticationPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl text-white font-semibold tracking-tight">
-                Login to an account
+                Create an account
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email and password below to login
+                Enter your email below to create your account
               </p>
             </div>
             <UserAuthForm />
